f06d41f8b10f6f42fcf3282b2b7897bb1425d6b0
kott <ott.kristian@gmail.com>
2013-09-06 16:20:52 -0400

    Added Default code igniter files

---
+++ application/views/main.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>LiveDescribe</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="icon" type="image/png" href="images/mic.png" />
+
+    <!-- Bootstrap CSS -->
+    <link href="./bootstrap/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    <link href="./bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+    
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="css/mediaStyles.css" />
+    <link rel="stylesheet" type="text/css" href="css/navigation.css" />
+
+    
+
+    <!-- JQUERY -->
+    <script src="http://code.jquery.com/jquery-1.8.3.js"></script>
+    <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+    
+    <!-- Bootstrap JS -->
+    <script src="./bootstrap/js/bootstrap.js"></script>
+    
+    <!--Main site calls, for login etc.-->
+    <script type="text/javascript" src="./js/siteOperations.js"></script>
+  </head>
+
+  <body>
+
+    <?php 
+      include("./assests/include/header.php");
+    ?>
+
+    <div class="container">
+
+      
+      
+
+    </div> <!-- /container -->
+
+  </body>
+</html>
