d8f75836aed883f7f246434c7aa4138d85ca51e2
kott <ott.kristian@gmail.com>
2013-09-06 17:54:45 -0400

    Added home page and some old resources

---
+++ application/controllers/main.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 */
+	public function index()
+	{
+		$this->load->view('main');
+	}
+}
+
+/* End of file main.php */
+/* Location: ./application/controllers/main.php */
--- application/views/main.php
+++ application/views/main.php
@@ -7,21 +7,20 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <link rel="icon" type="image/png" href="images/mic.png" />
+    <link rel="icon" type="image/png" href="assets/img/mic.png" />
 
     <!-- Bootstrap CSS -->
-    <link href="./bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/lib/bootstrap/css/bootstrap.css" rel="stylesheet">
     <style type="text/css">
       body {
         padding-top: 60px;
         padding-bottom: 40px;
       }
     </style>
-    <link href="./bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="assets/lib/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
     
     <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="css/mediaStyles.css" />
-    <link rel="stylesheet" type="text/css" href="css/navigation.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/header.css" />
 
     
 
@@ -30,22 +29,21 @@
     <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
     
     <!-- Bootstrap JS -->
-    <script src="./bootstrap/js/bootstrap.js"></script>
+    <script src="assets/lib/bootstrap/js/bootstrap.js"></script>
     
     <!--Main site calls, for login etc.-->
-    <script type="text/javascript" src="./js/siteOperations.js"></script>
+    <script type="text/javascript" src=""></script>
+
   </head>
 
   <body>
 
     <?php 
-      include("./assests/include/header.php");
+     include("assets/include/header.php");
     ?>
 
     <div class="container">
 
-      
-      
 
     </div> <!-- /container -->
 
