35087ea3a198c9447edd207ed8144ee7468eb666
kott <ott.kristian@gmail.com>
2013-09-24 14:48:54 -0400

    Started user_model for IMDC users

---
+++ application/models/user_model.php
@@ -0,0 +1,22 @@
+<?php
+class User_Model extends CI_Model {
+
+	
+
+	public function __construct(){
+		// Call the Model constructor
+		parent::__construct();
+		$this->load->database();
+	}
+
+	/**
+	*	login as IMDC user
+	*/
+	public function login(){
+		
+	}
+
+
+   
+}
+?>
